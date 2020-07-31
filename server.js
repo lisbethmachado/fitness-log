7055029f1e2c11350c5b1ddc76235e4ae7224f87
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-07-31 09:44:57 -0700

    added mongoose connection

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
 
 const app = express();
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
