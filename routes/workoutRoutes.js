e1146a0098c9a6d8d0e477175d52bce7e6ab1ea2
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-08-04 19:22:42 -0700

    changed models variable to global db in workoutRoutes

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,27 +1,27 @@
 const router = require('express').Router();
-const Workout = require('../models');
+const db = require('../models');
 
 router.get('/api/workouts', (request, response) => {
-  Workout.findAll()
+  db.Workout.findAll()
   .then( workouts => response.json(workouts))
   .catch( error => console.error(error))
 });
 
 router.post('/api/workouts', (request, response) => {
-  Workout.create(request.body)
+  db.Workout.create(request.body)
   .then( () => response.sendStatus(200))
   .catch( error => console.error(error))
 });
 
 router.put('/api/workouts/:id', (request, response) => {
-    Workout.findOneAndUpdate({_id: request.params.id}, request.body)
+    db.Workout.findOneAndUpdate({_id: request.params.id}, request.body)
     .then( () => response.sendStatus(200))
     .catch(error => response.sendStatus(400))
   });
   
   //delete a workout
   router.delete('/api/workouts/:id', (request, response) => {
-    Workout.deleteOne({_id: request.params.id})
+    db.Workout.deleteOne({_id: request.params.id})
     .then( () => response.sendStatus(200))
     .catch(error => response.sendStatus(400))
   });
