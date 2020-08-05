d5adb6815776fda89ce1acb5c07c2aa5bcfdb9e9
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-08-04 18:34:08 -0700

    added post route to workoutroutes js

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -2,9 +2,15 @@ const router = require('express').Router()
 const Workout = require('../models')
 
 router.get('/workouts', (request, response) => {
-  Workout.find()
-  .then( workouts => response.json(workouts))
-  .catch( error => console.error(error))
+    Workout.find()
+        .then(workouts => response.json(workouts))
+        .catch(error => console.error(error))
+});
+
+router.post('/workouts', (request, response) => {
+    Workout.create(request.body)
+        .then(() => response.sendStatus(200))
+        .catch(error => console.error(error))
 });
 
 module.exports = router
