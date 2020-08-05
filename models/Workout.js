0ccae42126afda09a67f68dc0d0385dbddda989d
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-08-05 16:25:09 -0700

    fixed workout model and fixed api get route bug

--- models/Workout.js
+++ models/Workout.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const Workout = mongoose.model('workout', new mongoose.Schema({
-  exercises:[{
-    type: {type: String},
+const workoutSchema = new Schema({
+  // day: new Date(new Date().setDate(new Date().getDate() - 9)),
+  exercises: [{
+    type: String,
     name: String,
     duration: Number,
     weight: Number,
@@ -10,7 +12,20 @@ const Workout = mongoose.model('workout', new mongoose.Schema({
     sets: Number,
     distance: Number
   }]
-}));
+});
 
 
+// const Workout = mongoose.model('Workout', new mongoose.Schema({
+//   exercises:[{
+//     type: {type: String},
+//     name: String,
+//     duration: Number,
+//     weight: Number,
+//     reps: Number,
+//     sets: Number,
+//     distance: Number
+//   }]
+// }));
+
+const Workout = mongoose.model("Workout", workoutSchema);
 module.exports = Workout;
--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,29 +1,37 @@
 const router = require('express').Router();
-const db = require('../models');
+const Workout = require('../models/Workout'); 
 
-router.get('/api/workouts', (request, response) => {
-  db.Workout.find.sort({ day: 1 })(request.body)
-  .then( workouts => response.json(workouts))
-  .catch( error => console.error(error))
-});
+router.get('/api/workouts', function (request, response) {
+  Workout.find({}, null, {sort: 'date'}, function(err, workouts){
+    if (err){
+      console.error(error)
+    }
 
-router.post('/api/workouts', (request, response) => {
-  db.Workout.create(request.body)
-  .then( () => response.sendStatus(200))
-  .catch( error => console.error(error))
-});
-
-router.put('/api/workouts/:id', (request, response) => {
-    db.Workout.findOneAndUpdate({_id: request.params.id}, request.body)
-    .then( () => response.sendStatus(200))
-    .catch(error => response.sendStatus(400))
+    response.json(workouts)
   });
   
-  //delete a workout
-  router.delete('/api/workouts/:id', (request, response) => {
-    db.Workout.deleteOne({_id: request.params.id})
-    .then( () => response.sendStatus(200))
-    .catch(error => response.sendStatus(400))
-  });
+  //(request.body)
+  //.then( workouts => response.json(workouts))
+  //.catch( error => console.error(error))
+});
+
+// router.post('/api/workouts', (request, response) => {
+//   db.Workout.create(request.body)
+//   .then( () => response.sendStatus(200))
+//   .catch( error => console.error(error))
+// });
+
+// router.put('/api/workouts/:id', (request, response) => {
+//     db.Workout.findOneAndUpdate({_id: request.params.id}, request.body)
+//     .then( () => response.sendStatus(200))
+//     .catch(error => response.sendStatus(400))
+//   });
+  
+//   //delete a workout
+//   router.delete('/api/workouts/:id', (request, response) => {
+//     db.Workout.deleteOne({_id: request.params.id})
+//     .then( () => response.sendStatus(200))
+//     .catch(error => response.sendStatus(400))
+//   });
 
 module.exports = router;
