71ba53a25ea062eee95bc0e8d6efab7157cca252
Lisbeth Machado <lisbeth@MacBook-Pro.local>
2020-08-04 19:20:20 -0700

    changed workoutRoutes to restful naming path api

--- routes/workoutRoutes.js
+++ routes/workoutRoutes.js
@@ -1,27 +1,26 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const Workout = require('../models');
 
-router.get('/workouts', (request, response) => {
+router.get('/api/workouts', (request, response) => {
   Workout.findAll()
   .then( workouts => response.json(workouts))
   .catch( error => console.error(error))
 });
 
-router.post('/workouts', (request, response) => {
+router.post('/api/workouts', (request, response) => {
   Workout.create(request.body)
   .then( () => response.sendStatus(200))
   .catch( error => console.error(error))
 });
 
-router.put('/workouts/:id', (request, response) => {
+router.put('/api/workouts/:id', (request, response) => {
     Workout.findOneAndUpdate({_id: request.params.id}, request.body)
     .then( () => response.sendStatus(200))
     .catch(error => response.sendStatus(400))
   });
   
   //delete a workout
-  router.delete('/workouts/:id', (request, response) => {
+  router.delete('/api/workouts/:id', (request, response) => {
     Workout.deleteOne({_id: request.params.id})
     .then( () => response.sendStatus(200))
     .catch(error => response.sendStatus(400))
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const {join} = require('path')
+const {join} = require("path")
 const mongoose = require("mongoose");
+const apiRouter = requre("./routes/workoutRoutes")
 
 
 const PORT = process.env.PORT || 3000;
@@ -16,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(join(__dirname, 'public')))
 
-app.use(require('./routes'));
+app.use(apiRouter);
 
 app.get('/', (request, response) => {
   response.sendFile(join(__dirname, 'public', 'index.html'))
